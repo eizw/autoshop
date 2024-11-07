d4a0a5f4e67488eab78d741ca219d2fb744d0663
eizw <hamzahamri2005@gmail.com>
2024-11-07 10:54:36 +0800

    Added images

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def main():
     return flask.render_template("index.html")
 
+@app.route('/items')
+def item_list():
+    return flask.render_template("item_list.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
