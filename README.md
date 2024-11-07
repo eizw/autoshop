d35c7691843739be6131a45df048a6ab3e9632a8
eizw <hamzahamri2005@gmail.com>
2024-11-07 10:29:32 +0800

    Added main page Updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # autoshop
+## Make sure you're in the root directory!
+1. python -m venv venv
+2. pip install -r requirements.txt
+3. ./app.py
--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import flask
 
 app = flask.Flask(__name__)
+app.jinja_env.auto_reload = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/')
 def main():
     return flask.render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
