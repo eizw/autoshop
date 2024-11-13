ca99ba3532b89003549b5cdc334f1093da9463b9
eizw <hamzahamri2005@gmail.com>
2024-11-13 19:14:31 +0800

    Added items Added item list page Added login, register, item_list pages (Naveen)

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import flask
+import json
 
 app = flask.Flask(__name__)
 app.jinja_env.auto_reload = True
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+all_items = {}
+f = open('items.json')
+all_items = json.load(f)
 
 @app.route('/')
 def main():
@@ -10,7 +15,18 @@ def main():
 
 @app.route('/items')
 def item_list():
-    return flask.render_template("item_list.html")
+    query = flask.request.args.get('category', '')
+    categories = all_items.keys()
+    if query:
+        items = all_items[query]
+    else:
+        items = sum(all_items.values(), [])
+
+    return flask.render_template("item_list.html", 
+        items=items,
+        categories=categories,
+        current_category=query,
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
